01da57aa2f02bc17919f256a4d019b6cd5b447da
zurexsama <zurexsama@gmail.com>
2026-02-02 19:16:06 +0800

    Update changes

--- app.py
+++ app.py
@@ -722,9 +722,7 @@ def quiz():
 
 
 
-if __name__ == "__main__":
-    port = int(os.getenv('PORT', 5000))
-    debug = os.getenv('FLASK_ENV') == 'development'
-    app.run(host='0.0.0.0', port=port, debug=debug)
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
